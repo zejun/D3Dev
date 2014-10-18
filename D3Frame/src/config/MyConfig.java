bdbb545fe43c4ab9d549060296a0be4b4d3a904e
Eric <eric900130@gmail.com>
2014-10-18 14:33:57 -0400

    update with new config file

---
+++ D3Frame/src/config/MyConfig.java
@@ -0,0 +1,41 @@
+package config;
+
+import com.jfinal.config.*;
+
+import demo.HelloController;
+
+public class MyConfig extends JFinalConfig{
+
+	@Override
+	public void configConstant(Constants me) {
+		// TODO Auto-generated method stub
+		me.setDevMode(true);
+	}
+
+	@Override
+	public void configHandler(Handlers arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configInterceptor(Interceptors arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configPlugin(Plugins arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configRoute(Routes me) {
+		// TODO Auto-generated method stub
+		me.add("/hello",demo.HelloController.class);
+	}
+
+	
+	
+}
